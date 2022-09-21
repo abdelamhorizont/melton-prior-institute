20dfe5438d636126ace186b732dedc7f67312939
abdelamhorizont <abdelhamid.ameur@hotmail.de>
2022-09-21 16:55:37 +0200

    rm createSchemaCustomization

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,249 +17,249 @@
 //     },
 //   })
 
-  actions.createFieldExtension({
-    name: "wpRecursiveImage",
-    extend(options) {
-      return {
-        async resolve(source, args, context, info) {
-          return source
-        },
-      }
-    },
-  })
+//   actions.createFieldExtension({
+//     name: "wpRecursiveImage",
+//     extend(options) {
+//       return {
+//         async resolve(source, args, context, info) {
+//           return source
+//         },
+//       }
+//     },
+//   })
 
-  // interfaces
-  actions.createTypes(/* GraphQL */ `
-    interface HomepageImage implements Node {
-      id: ID!
-      alt: String
-      gatsbyImageData: JSON
-      image: HomepageImage
-      localFile: File
-      url: String
-    }
+//   // interfaces
+//   actions.createTypes(/* GraphQL */ `
+//     interface HomepageImage implements Node {
+//       id: ID!
+//       alt: String
+//       gatsbyImageData: JSON
+//       image: HomepageImage
+//       localFile: File
+//       url: String
+//     }
 
-    interface HomepageBlock implements Node {
-      id: ID!
-      blocktype: String
-    }
-  `)
+//     interface HomepageBlock implements Node {
+//       id: ID!
+//       blocktype: String
+//     }
+//   `)
 
-  // blocks
-  actions.createTypes(/* GraphQL */ `
-    type HomepageLink implements Node {
-      id: ID!
-      href: String
-      text: String
-    }
+//   // blocks
+//   actions.createTypes(/* GraphQL */ `
+//     type HomepageLink implements Node {
+//       id: ID!
+//       href: String
+//       text: String
+//     }
 
-    type HomepageHero implements Node & HomepageBlock {
-      id: ID!
-      blocktype: String
-      heading: String!
-      kicker: String
-      subhead: String
-      image: HomepageImage @link
-      text: String
-      links: [HomepageLink] @link
-    }
+//     type HomepageHero implements Node & HomepageBlock {
+//       id: ID!
+//       blocktype: String
+//       heading: String!
+//       kicker: String
+//       subhead: String
+//       image: HomepageImage @link
+//       text: String
+//       links: [HomepageLink] @link
+//     }
 
-    type HomepageCta implements Node & HomepageBlock {
-      id: ID!
-      blocktype: String
-      kicker: String
-      heading: String
-      text: String
-      links: [HomepageLink] @link
-      image: HomepageImage @link
-    }
+//     type HomepageCta implements Node & HomepageBlock {
+//       id: ID!
+//       blocktype: String
+//       kicker: String
+//       heading: String
+//       text: String
+//       links: [HomepageLink] @link
+//       image: HomepageImage @link
+//     }
 
-    type HomepageFeature implements Node & HomepageBlock {
-      id: ID!
-      blocktype: String
-      heading: String
-      kicker: String
-      text: String
-      image: HomepageImage @link
-      links: [HomepageLink] @link
-    }
+//     type HomepageFeature implements Node & HomepageBlock {
+//       id: ID!
+//       blocktype: String
+//       heading: String
+//       kicker: String
+//       text: String
+//       image: HomepageImage @link
+//       links: [HomepageLink] @link
+//     }
 
-    type HomepageTestimonial implements Node {
-      id: ID!
-      quote: String
-      source: String
-      avatar: HomepageImage @link
-    }
+//     type HomepageTestimonial implements Node {
+//       id: ID!
+//       quote: String
+//       source: String
+//       avatar: HomepageImage @link
+//     }
 
-    type HomepageBenefit implements Node {
-      id: ID!
-      heading: String
-      text: String
-      image: HomepageImage @link
-    }
+//     type HomepageBenefit implements Node {
+//       id: ID!
+//       heading: String
+//       text: String
+//       image: HomepageImage @link
+//     }
 
-    type HomepageLogo implements Node {
-      id: ID!
-      image: HomepageImage @link
-      alt: String @proxy(from: "image.title")
-    }
+//     type HomepageLogo implements Node {
+//       id: ID!
+//       image: HomepageImage @link
+//       alt: String @proxy(from: "image.title")
+//     }
 
-    type HomepageProduct implements Node {
-      id: ID!
-      heading: String
-      text: String
-      image: HomepageImage @link
-      links: [HomepageLink] @link
-    }
+//     type HomepageProduct implements Node {
+//       id: ID!
+//       heading: String
+//       text: String
+//       image: HomepageImage @link
+//       links: [HomepageLink] @link
+//     }
 
-    type HomepageFeatureList implements Node & HomepageBlock {
-      id: ID!
-      blocktype: String
-      kicker: String
-      heading: String
-      text: String
-      content: [HomepageFeature] @link
-    }
+//     type HomepageFeatureList implements Node & HomepageBlock {
+//       id: ID!
+//       blocktype: String
+//       kicker: String
+//       heading: String
+//       text: String
+//       content: [HomepageFeature] @link
+//     }
 
-    type HomepageLogoList implements Node & HomepageBlock {
-      id: ID!
-      blocktype: String
-      text: String
-      logos: [HomepageImage] @link
-    }
+//     type HomepageLogoList implements Node & HomepageBlock {
+//       id: ID!
+//       blocktype: String
+//       text: String
+//       logos: [HomepageImage] @link
+//     }
 
-    type HomepageTestimonialList implements Node & HomepageBlock {
-      id: ID!
-      blocktype: String
-      kicker: String
-      heading: String
-      content: [HomepageTestimonial] @link
-    }
+//     type HomepageTestimonialList implements Node & HomepageBlock {
+//       id: ID!
+//       blocktype: String
+//       kicker: String
+//       heading: String
+//       content: [HomepageTestimonial] @link
+//     }
 
-    type HomepageBenefitList implements Node & HomepageBlock {
-      id: ID!
-      blocktype: String
-      heading: String
-      text: String
-      content: [HomepageBenefit] @link
-    }
+//     type HomepageBenefitList implements Node & HomepageBlock {
+//       id: ID!
+//       blocktype: String
+//       heading: String
+//       text: String
+//       content: [HomepageBenefit] @link
+//     }
 
-    type HomepageStat implements Node {
-      id: ID!
-      value: String
-      label: String
-      heading: String
-    }
+//     type HomepageStat implements Node {
+//       id: ID!
+//       value: String
+//       label: String
+//       heading: String
+//     }
 
-    type HomepageStatList implements Node & HomepageBlock {
-      id: ID!
-      blocktype: String
-      kicker: String
-      heading: String
-      text: String
-      image: HomepageImage @link
-      icon: HomepageImage @link
-      content: [HomepageStat] @link
-      links: [HomepageLink] @link
-    }
+//     type HomepageStatList implements Node & HomepageBlock {
+//       id: ID!
+//       blocktype: String
+//       kicker: String
+//       heading: String
+//       text: String
+//       image: HomepageImage @link
+//       icon: HomepageImage @link
+//       content: [HomepageStat] @link
+//       links: [HomepageLink] @link
+//     }
 
-    type HomepageProductList implements Node & HomepageBlock {
-      id: ID!
-      blocktype: String
-      kicker: String
-      heading: String
-      text: String
-      content: [HomepageProduct] @link
-    }
+//     type HomepageProductList implements Node & HomepageBlock {
+//       id: ID!
+//       blocktype: String
+//       kicker: String
+//       heading: String
+//       text: String
+//       content: [HomepageProduct] @link
+//     }
 
-    type AboutHero implements Node & HomepageBlock {
-      id: ID!
-      blocktype: String
-      heading: String
-      text: String
-      image: HomepageImage @link
-    }
+//     type AboutHero implements Node & HomepageBlock {
+//       id: ID!
+//       blocktype: String
+//       heading: String
+//       text: String
+//       image: HomepageImage @link
+//     }
 
-    type AboutStat implements Node & HomepageBlock {
-      id: ID!
-      blocktype: String
-      value: String
-      label: String
-    }
+//     type AboutStat implements Node & HomepageBlock {
+//       id: ID!
+//       blocktype: String
+//       value: String
+//       label: String
+//     }
 
-    type AboutStatList implements Node & HomepageBlock {
-      id: ID!
-      blocktype: String
-      content: [AboutStat] @link
-    }
+//     type AboutStatList implements Node & HomepageBlock {
+//       id: ID!
+//       blocktype: String
+//       content: [AboutStat] @link
+//     }
 
-    type AboutProfile implements Node {
-      id: ID!
-      image: HomepageImage @link
-      name: String
-      jobTitle: String
-    }
+//     type AboutProfile implements Node {
+//       id: ID!
+//       image: HomepageImage @link
+//       name: String
+//       jobTitle: String
+//     }
 
-    type AboutLeadership implements Node & HomepageBlock {
-      id: ID!
-      blocktype: String
-      kicker: String
-      heading: String
-      subhead: String
-      content: [AboutProfile] @link
-    }
+//     type AboutLeadership implements Node & HomepageBlock {
+//       id: ID!
+//       blocktype: String
+//       kicker: String
+//       heading: String
+//       subhead: String
+//       content: [AboutProfile] @link
+//     }
 
-    type AboutLogoList implements Node & HomepageBlock {
-      id: ID!
-      blocktype: String
-      heading: String
-      links: [HomepageLink] @link
-      logos: [HomepageImage] @link
-    }
-  `)
+//     type AboutLogoList implements Node & HomepageBlock {
+//       id: ID!
+//       blocktype: String
+//       heading: String
+//       links: [HomepageLink] @link
+//       logos: [HomepageImage] @link
+//     }
+//   `)
 
-  // pages
-  actions.createTypes(/* GraphQL */ `
-    type Homepage implements Node {
-      id: ID!
-      title: String
-      description: String
-      image: HomepageImage @link
-      content: [HomepageBlock] @link
-    }
+//   // pages
+//   actions.createTypes(/* GraphQL */ `
+//     type Homepage implements Node {
+//       id: ID!
+//       title: String
+//       description: String
+//       image: HomepageImage @link
+//       content: [HomepageBlock] @link
+//     }
 
-    type AboutPage implements Node {
-      id: ID!
-      title: String
-      description: String
-      image: HomepageImage @link
-      content: [HomepageBlock] @link
-    }
+//     type AboutPage implements Node {
+//       id: ID!
+//       title: String
+//       description: String
+//       image: HomepageImage @link
+//       content: [HomepageBlock] @link
+//     }
 
-    type Page implements Node {
-      id: ID!
-      slug: String!
-      title: String
-      description: String
-      image: HomepageImage @link
-      html: String
-    }
-  `)
+//     type Page implements Node {
+//       id: ID!
+//       slug: String!
+//       title: String
+//       description: String
+//       image: HomepageImage @link
+//       html: String
+//     }
+//   `)
 
-  // WordPress types
-  actions.createTypes(/* GraphQL */ `
-    type WpMediaItem implements Node & HomepageImage {
-      id: ID!
-      alt: String @proxy(from: "altText")
-      altText: String
-      gatsbyImageData: JSON @wpImagePassthroughResolver
-      image: HomepageImage @wpRecursiveImage
-      localFile: File
-      url: String @proxy(from: "mediaItemUrl")
-      mediaItemUrl: String
-    }
-  `)
-}
+//   // WordPress types
+//   actions.createTypes(/* GraphQL */ `
+//     type WpMediaItem implements Node & HomepageImage {
+//       id: ID!
+//       alt: String @proxy(from: "altText")
+//       altText: String
+//       gatsbyImageData: JSON @wpImagePassthroughResolver
+//       image: HomepageImage @wpRecursiveImage
+//       localFile: File
+//       url: String @proxy(from: "mediaItemUrl")
+//       mediaItemUrl: String
+//     }
+//   `)
+// }
 
 exports.onCreateNode = ({
   node,
