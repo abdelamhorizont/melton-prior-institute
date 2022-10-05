a145b09573f3fa581d1e94ec1e161fecb63bffd7
abdelamhorizont <abdelhamid.ameur@hotmail.de>
2022-10-05 10:44:18 +0200

    netlify-plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,6 +84,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-vanilla-extract",
     `gatsby-plugin-sass`,
+    `gatsby-plugin-netlify`,
     // {
     //   resolve: "gatsby-plugin-manifest",
     //   options: {
