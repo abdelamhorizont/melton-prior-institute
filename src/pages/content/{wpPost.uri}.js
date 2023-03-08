b8c97ae1e754b5b4d4096d34ddb366e564a00336
abdelamhorizont <abdelhamid.ameur@hotmail.de>
2023-03-08 11:34:25 +0900

    now?

--- src/pages/content/{wpPost.uri}.js
+++ src/pages/content/{wpPost.uri}.js
@@ -150,7 +150,7 @@ export default function Post({ data }) {
                     const src = domNode.children[1]?.children[1]?.children[0]?.attribs["src"].replace('http', 'https') || domNode.children[0]?.attribs["src"] || domNode.children[0]?.attribs["href"]
                     // const srcset = domNode.children[1]?.children[1]?.children[0]?.attribs["srcset"].replace('http', 'https') || domNode.children[0]?.attribs["srcset"]
                     const caption = domNode.children[3]?.children[0]?.data || domNode.children[1]?.children[0]?.data 
-                    console.log(domNode)
+                    // console.log(domNode)
                     return (
                       <figure className="gallery-item">
                         <Item
