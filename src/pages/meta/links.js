e6e0ef3abe88f05d18e3bf6fc682aeb240cfcb08
burritotrex <mike_razniewski@hotmail.de>
2023-07-04 18:18:38 +0200

    update

--- src/pages/meta/links.js
+++ src/pages/meta/links.js
@@ -12,27 +12,55 @@ import {
 } from '../../styles/content.module.scss'
 
 
+
+
 export default function Links() {
   const data = useStaticQuery(graphql`
     query {
         wpPage(title: {eq: "Links"}) {
             content
+            id
+          }
+          allWpPage(filter: {parentId: {eq: ""}}) {
+            nodes {
+              id
+              title
+              content
+            }
           }
         }   
     `)
 
+  const parentPage = data.wpPage
+  const childrenPages = data.allWpPage.nodes
+
+  console.log('parnpag:', parentPage, 'childrenPages:', childrenPages )
+
+
   return (
     <Layout>
-    <div className={contentPage}>
-      <div class={articleWrapper}>
-        <div class={sectionTitle}><h4>Links</h4></div>
-      <div className={topBrackets}>
-        <span>]</span>
-        <span>[</span>
+      <div className={contentPage}>
+        <div className={articleWrapper}>
+          <div className={sectionTitle}><h4>Links</h4></div>
+          <div className={topBrackets}>
+            <span>]</span>
+            <span>[</span>
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: parentPage.content }} className={pageContent} />
+        </div>
+
+
+        {childrenPages.map(page => (
+          <div key={page.title} className={articleWrapper}>
+            <div className={sectionTitle}><h4>{page.title}</h4></div>
+            <div className={topBrackets}>
+              <span>]</span>
+              <span>[</span>
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: page.content }} className={pageContent} />
+          </div>
+        ))}
       </div>
-      <div dangerouslySetInnerHTML={{ __html: data.wpPage.content }} className={pageContent} />
-      </div>
-    </div>
-  </Layout>
+    </Layout>
   )
 }
