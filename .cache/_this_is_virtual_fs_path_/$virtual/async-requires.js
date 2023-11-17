db57e676c2e48918a2286d1c3337ffe34895e659
burritotrex <mike_razniewski@hotmail.de>
2023-11-17 13:54:55 +0100

    test

--- .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
@@ -1,15 +1,32 @@
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-content-collections-js": () => import("./../../../src/pages/content/collections.js" /* webpackChunkName: "component---src-pages-content-collections-js" */),
-  "component---src-pages-content-features-js": () => import("./../../../src/pages/content/features.js" /* webpackChunkName: "component---src-pages-content-features-js" */),
-  "component---src-pages-content-pictorials-js": () => import("./../../../src/pages/content/pictorials.js" /* webpackChunkName: "component---src-pages-content-pictorials-js" */),
-  "component---src-pages-content-search-results-js": () => import("./../../../src/pages/content/searchResults.js" /* webpackChunkName: "component---src-pages-content-search-results-js" */),
-  "component---src-pages-content-wp-post-uri-js": () => import("./../../../src/pages/content/{wpPost.uri}.js" /* webpackChunkName: "component---src-pages-content-wp-post-uri-js" */),
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-meta-about-js": () => import("./../../../src/pages/meta/about.js" /* webpackChunkName: "component---src-pages-meta-about-js" */),
-  "component---src-pages-meta-agb-js": () => import("./../../../src/pages/meta/agb.js" /* webpackChunkName: "component---src-pages-meta-agb-js" */),
-  "component---src-pages-meta-contact-js": () => import("./../../../src/pages/meta/contact.js" /* webpackChunkName: "component---src-pages-meta-contact-js" */),
-  "component---src-pages-meta-imprint-js": () => import("./../../../src/pages/meta/imprint.js" /* webpackChunkName: "component---src-pages-meta-imprint-js" */)
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=default" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js?export=default" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-content-collections-js": () => import("./../../../src/pages/content/collections.js?export=default" /* webpackChunkName: "component---src-pages-content-collections-js" */),
+  "component---src-pages-content-features-js": () => import("./../../../src/pages/content/features.js?export=default" /* webpackChunkName: "component---src-pages-content-features-js" */),
+  "component---src-pages-content-pictorials-js": () => import("./../../../src/pages/content/pictorials.js?export=default" /* webpackChunkName: "component---src-pages-content-pictorials-js" */),
+  "component---src-pages-content-search-results-js": () => import("./../../../src/pages/content/searchResults.js?export=default" /* webpackChunkName: "component---src-pages-content-search-results-js" */),
+  "component---src-pages-content-wp-post-uri-js": () => import("./../../../src/pages/content/{wpPost.uri}.js?export=default" /* webpackChunkName: "component---src-pages-content-wp-post-uri-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js?export=default" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-meta-about-js": () => import("./../../../src/pages/meta/about.js?export=default" /* webpackChunkName: "component---src-pages-meta-about-js" */),
+  "component---src-pages-meta-agb-js": () => import("./../../../src/pages/meta/agb.js?export=default" /* webpackChunkName: "component---src-pages-meta-agb-js" */),
+  "component---src-pages-meta-contact-js": () => import("./../../../src/pages/meta/contact.js?export=default" /* webpackChunkName: "component---src-pages-meta-contact-js" */),
+  "component---src-pages-meta-imprint-js": () => import("./../../../src/pages/meta/imprint.js?export=default" /* webpackChunkName: "component---src-pages-meta-imprint-js" */)
+}
+
+
+
+exports.head = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=head" /* webpackChunkName: "component---cache-dev-404-page-jshead" */),
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js?export=head" /* webpackChunkName: "component---src-pages-404-jshead" */),
+  "component---src-pages-content-collections-js": () => import("./../../../src/pages/content/collections.js?export=head" /* webpackChunkName: "component---src-pages-content-collections-jshead" */),
+  "component---src-pages-content-features-js": () => import("./../../../src/pages/content/features.js?export=head" /* webpackChunkName: "component---src-pages-content-features-jshead" */),
+  "component---src-pages-content-pictorials-js": () => import("./../../../src/pages/content/pictorials.js?export=head" /* webpackChunkName: "component---src-pages-content-pictorials-jshead" */),
+  "component---src-pages-content-search-results-js": () => import("./../../../src/pages/content/searchResults.js?export=head" /* webpackChunkName: "component---src-pages-content-search-results-jshead" */),
+  "component---src-pages-content-wp-post-uri-js": () => import("./../../../src/pages/content/{wpPost.uri}.js?export=head" /* webpackChunkName: "component---src-pages-content-wp-post-uri-jshead" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js?export=head" /* webpackChunkName: "component---src-pages-index-jshead" */),
+  "component---src-pages-meta-about-js": () => import("./../../../src/pages/meta/about.js?export=head" /* webpackChunkName: "component---src-pages-meta-about-jshead" */),
+  "component---src-pages-meta-agb-js": () => import("./../../../src/pages/meta/agb.js?export=head" /* webpackChunkName: "component---src-pages-meta-agb-jshead" */),
+  "component---src-pages-meta-contact-js": () => import("./../../../src/pages/meta/contact.js?export=head" /* webpackChunkName: "component---src-pages-meta-contact-jshead" */),
+  "component---src-pages-meta-imprint-js": () => import("./../../../src/pages/meta/imprint.js?export=head" /* webpackChunkName: "component---src-pages-meta-imprint-jshead" */)
 }
 
--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -4,17 +4,17 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/Melton Prior/melton-prior-institute/.cache/dev-404-page.js")),
-  "component---src-pages-404-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/Melton Prior/melton-prior-institute/src/pages/404.js")),
-  "component---src-pages-content-collections-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/Melton Prior/melton-prior-institute/src/pages/content/collections.js")),
-  "component---src-pages-content-features-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/Melton Prior/melton-prior-institute/src/pages/content/features.js")),
-  "component---src-pages-content-pictorials-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/Melton Prior/melton-prior-institute/src/pages/content/pictorials.js")),
-  "component---src-pages-content-search-results-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/Melton Prior/melton-prior-institute/src/pages/content/searchResults.js")),
-  "component---src-pages-content-wp-post-uri-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/Melton Prior/melton-prior-institute/src/pages/content/{wpPost.uri}.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/Melton Prior/melton-prior-institute/src/pages/index.js")),
-  "component---src-pages-meta-about-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/Melton Prior/melton-prior-institute/src/pages/meta/about.js")),
-  "component---src-pages-meta-agb-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/Melton Prior/melton-prior-institute/src/pages/meta/agb.js")),
-  "component---src-pages-meta-contact-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/Melton Prior/melton-prior-institute/src/pages/meta/contact.js")),
-  "component---src-pages-meta-imprint-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/Melton Prior/melton-prior-institute/src/pages/meta/imprint.js"))
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/fxxx0cream/Pictures/03_commercial/37_Melton_Prior_Institute/03_implementation/coding_rep/melton-prior-institute-2/melton-prior-institute/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/fxxx0cream/Pictures/03_commercial/37_Melton_Prior_Institute/03_implementation/coding_rep/melton-prior-institute-2/melton-prior-institute/src/pages/404.js")),
+  "component---src-pages-content-collections-js": preferDefault(require("/Users/fxxx0cream/Pictures/03_commercial/37_Melton_Prior_Institute/03_implementation/coding_rep/melton-prior-institute-2/melton-prior-institute/src/pages/content/collections.js")),
+  "component---src-pages-content-features-js": preferDefault(require("/Users/fxxx0cream/Pictures/03_commercial/37_Melton_Prior_Institute/03_implementation/coding_rep/melton-prior-institute-2/melton-prior-institute/src/pages/content/features.js")),
+  "component---src-pages-content-pictorials-js": preferDefault(require("/Users/fxxx0cream/Pictures/03_commercial/37_Melton_Prior_Institute/03_implementation/coding_rep/melton-prior-institute-2/melton-prior-institute/src/pages/content/pictorials.js")),
+  "component---src-pages-content-search-results-js": preferDefault(require("/Users/fxxx0cream/Pictures/03_commercial/37_Melton_Prior_Institute/03_implementation/coding_rep/melton-prior-institute-2/melton-prior-institute/src/pages/content/searchResults.js")),
+  "component---src-pages-content-wp-post-uri-js": preferDefault(require("/Users/fxxx0cream/Pictures/03_commercial/37_Melton_Prior_Institute/03_implementation/coding_rep/melton-prior-institute-2/melton-prior-institute/src/pages/content/{wpPost.uri}.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/fxxx0cream/Pictures/03_commercial/37_Melton_Prior_Institute/03_implementation/coding_rep/melton-prior-institute-2/melton-prior-institute/src/pages/index.js")),
+  "component---src-pages-meta-about-js": preferDefault(require("/Users/fxxx0cream/Pictures/03_commercial/37_Melton_Prior_Institute/03_implementation/coding_rep/melton-prior-institute-2/melton-prior-institute/src/pages/meta/about.js")),
+  "component---src-pages-meta-agb-js": preferDefault(require("/Users/fxxx0cream/Pictures/03_commercial/37_Melton_Prior_Institute/03_implementation/coding_rep/melton-prior-institute-2/melton-prior-institute/src/pages/meta/agb.js")),
+  "component---src-pages-meta-contact-js": preferDefault(require("/Users/fxxx0cream/Pictures/03_commercial/37_Melton_Prior_Institute/03_implementation/coding_rep/melton-prior-institute-2/melton-prior-institute/src/pages/meta/contact.js")),
+  "component---src-pages-meta-imprint-js": preferDefault(require("/Users/fxxx0cream/Pictures/03_commercial/37_Melton_Prior_Institute/03_implementation/coding_rep/melton-prior-institute-2/melton-prior-institute/src/pages/meta/imprint.js"))
 }
 
